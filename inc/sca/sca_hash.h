300ebeddcf380dfa09d62c0dedbafa0b592edd73
huowj <huowenjie0427@sina.com>
2022-07-27 07:35:48 +0000

    修改注释

--- inc/sca/sca_hash.h
+++ inc/sca/sca_hash.h
@@ -20,31 +20,31 @@ typedef void (*sca_hash_free_item)(SCA_BYTE *elem, int len);
 typedef void (*sca_hash_free_item_data)(struct sca_data *elem);
 
 /* 创建哈希表, 指定初始长度 */
-SCA_HASH_MAP *sca_hash_map_new(int len);
+extern SCA_HASH_MAP *sca_hash_map_new(int len);
 
 /* 释放哈希表 */
-void sca_hash_map_del(SCA_HASH_MAP *map);
+extern void sca_hash_map_del(SCA_HASH_MAP *map);
 
 /* 清空哈希表 */
-void sca_hash_map_clear(SCA_HASH_MAP *map);
+extern void sca_hash_map_clear(SCA_HASH_MAP *map);
 
 /* 清空哈希表并释放单个元素 */
-void sca_hash_map_clear_item(
+extern void sca_hash_map_clear_item(
     SCA_HASH_MAP *map,
     sca_hash_free_item free_item
 );
 
 /* 清空哈希表并释放数据块 */
-void sca_hash_map_clear_data(
+extern void sca_hash_map_clear_data(
     SCA_HASH_MAP *map,
     sca_hash_free_item_data free_data
 );
 
 /* 获取元素数量 */
-SCA_UINT32 sca_hash_map_node_count(SCA_HASH_MAP *map);
+extern SCA_UINT32 sca_hash_map_node_count(SCA_HASH_MAP *map);
 
 /* 插入元素, 允许 data 为空 */
-SCA_UINT32 sca_hash_map_insert(
+extern SCA_UINT32 sca_hash_map_insert(
     SCA_HASH_MAP *map,
     const char *key,
     SCA_BYTE *data,
@@ -52,14 +52,14 @@ SCA_UINT32 sca_hash_map_insert(
 );
 
 /* 插入元素, 对于 value 只保存地址信息，已存在的元素会直接被覆盖 */
-SCA_UINT32 sca_hash_map_insert_data(
+extern SCA_UINT32 sca_hash_map_insert_data(
     SCA_HASH_MAP *map,
     const char *key,
     struct sca_data *value
 );
 
 /* 移除元素，data 和 len 为出参可以为 NULL */
-SCA_UINT32 sca_hash_map_remove(
+extern SCA_UINT32 sca_hash_map_remove(
     SCA_HASH_MAP *map,
     const char *key,
     SCA_BYTE **data,
@@ -67,14 +67,14 @@ SCA_UINT32 sca_hash_map_remove(
 );
 
 /* 移除元素, value 为出参 */
-SCA_UINT32 sca_hash_map_remove_data(
+extern SCA_UINT32 sca_hash_map_remove_data(
     SCA_HASH_MAP *map,
     const char *key,
     struct sca_data *value
 );
 
 /* 检索元素 */
-SCA_UINT32 sca_hash_map_retrieve(
+extern SCA_UINT32 sca_hash_map_retrieve(
     SCA_HASH_MAP *map,
     const char *key,
     SCA_BYTE **data,
@@ -82,43 +82,43 @@ SCA_UINT32 sca_hash_map_retrieve(
 );
 
 /* 检索数据 */
-SCA_UINT32 sca_hash_map_retrieve_data(
+extern SCA_UINT32 sca_hash_map_retrieve_data(
     SCA_HASH_MAP *map,
     const char *key,
     struct sca_data *value
 );
 
 /* 获取迭代器 */
-SCA_MAP_ITERATOR *sca_hash_map_iterator(SCA_HASH_MAP *map);
+extern SCA_MAP_ITERATOR *sca_hash_map_iterator(SCA_HASH_MAP *map);
 
 /* 将迭代器指针定位到首个元素上 */
-SCA_MAP_ITERATOR *sca_hash_map_first(SCA_MAP_ITERATOR *it);
+extern SCA_MAP_ITERATOR *sca_hash_map_first(SCA_MAP_ITERATOR *it);
 
 /* 访问下一个元素，如果返回值为 NULL，则说明没有元素 */
-SCA_MAP_ITERATOR *sca_hash_map_next(SCA_MAP_ITERATOR *it);
+extern SCA_MAP_ITERATOR *sca_hash_map_next(SCA_MAP_ITERATOR *it);
 
 /* 获取键，len 为缓冲区长度，buff 为 NULL 时，len 返回缓冲区长度 */
-SCA_UINT32 sca_hash_get_key(SCA_MAP_ITERATOR *it, char *buff, int *len);
+extern SCA_UINT32 sca_hash_get_key(SCA_MAP_ITERATOR *it, char *buff, int *len);
 
 /* 获取值（直接赋值），value 和 len 皆可为 NULL */
-SCA_UINT32 sca_hash_get_value(
+extern SCA_UINT32 sca_hash_get_value(
     SCA_MAP_ITERATOR *it,
     SCA_BYTE **value,
     int *len
 );
 
 /* 获取二进制数据块 */
-SCA_UINT32 sca_hash_get_data(SCA_MAP_ITERATOR *it, struct sca_data *data);
+extern SCA_UINT32 sca_hash_get_data(SCA_MAP_ITERATOR *it, struct sca_data *data);
 
 /* 设置值，我们会重新覆盖原有的数据 */
-SCA_UINT32 sca_hash_set_value(
+extern SCA_UINT32 sca_hash_set_value(
     SCA_MAP_ITERATOR *it,
     SCA_BYTE *value,
     int len
 );
 
 /* 设置数据块 */
-SCA_UINT32 sca_hash_set_data(SCA_MAP_ITERATOR *it, struct sca_data *data);
+extern SCA_UINT32 sca_hash_set_data(SCA_MAP_ITERATOR *it, struct sca_data *data);
 
 #ifdef __cplusplus
 }
--- inc/sca/sca_link.h
+++ inc/sca/sca_link.h
@@ -28,40 +28,40 @@ struct sca_link_node
 };
 
 /* 链表还原 */
-void sca_link_reset(struct sca_link *link);
+extern void sca_link_reset(struct sca_link *link);
 
 /* 将节点接入尾部 */
-SCA_UINT32 sca_link_push(struct sca_link *link, struct sca_link_node *node);
+extern SCA_UINT32 sca_link_push(struct sca_link *link, struct sca_link_node *node);
 
 /* 按索引插入节点，如 0 则插在第一个，依次类推，查询节点复杂度为 O(n) */
-SCA_UINT32 sca_link_insert(
+extern SCA_UINT32 sca_link_insert(
     struct sca_link *link,
     int index,
     struct sca_link_node *node
 );
 
 /* 在目标节点之前插入节点, target 不能为空 */
-SCA_UINT32 sca_link_insert_before(
+extern SCA_UINT32 sca_link_insert_before(
     struct sca_link *link,
     struct sca_link_node *target,
     struct sca_link_node *node
 );
 
 /* 在目标节点之后插入节点, target 不能为空 */
-SCA_UINT32 sca_link_insert_after(
+extern SCA_UINT32 sca_link_insert_after(
     struct sca_link *link,
     struct sca_link_node *target,
     struct sca_link_node *node
 );
 
 /* 移除尾部节点，同时返回节点地址 */
-struct sca_link_node *sca_link_pop(struct sca_link *link);
+extern struct sca_link_node *sca_link_pop(struct sca_link *link);
 
 /* 根据索引移除节点，查询节点复杂度为 O(n) */
-struct sca_link_node *sca_link_remove(struct sca_link *link, int index);
+extern struct sca_link_node *sca_link_remove(struct sca_link *link, int index);
 
 /* 移除目标节点, 移除成功返回该节点, 移除时会先查询节点是否存在 */
-struct sca_link_node *sca_link_remove_node(
+extern struct sca_link_node *sca_link_remove_node(
     struct sca_link *link,
     struct sca_link_node *target
 );
@@ -72,7 +72,7 @@ struct sca_link_node *sca_link_remove_node(
  *
  * 调用本函数定要万分小心，确保 target 属于 link。
  */
-struct sca_link_node *sca_link_remove_force(
+extern struct sca_link_node *sca_link_remove_force(
     struct sca_link *link,
     struct sca_link_node *target
 );
